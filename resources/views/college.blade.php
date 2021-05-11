18b7ec4638c198d621e78d0ba69636f7688b5430
Akhilamk <mkakhi1992@gmail.com>
2021-05-10 23:56:35 -0700

    first commit

---
+++ resources/views/college.blade.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+ <link rel="stylesheet" href="{{url('style.css')}}">
+    <title>Welcome to E-learning</title>
+  </head>
+  <body>
+   <div class="container-fluid">
+       <div class="row">
+           <div class="col-sm-7" style="height: 75px; background-color: white;" > &nbsp;<b>BRILLIANCE<img src="{{url('images/clglog.jpg')}}" alt="" height="70px"  style="padding-left: 10px; margin-top: 5px;">&nbsp;&nbsp;ACADEMY</b></div>
+           <div class="col-sm-5" style="height: 75px; background-color: white;" ><button class="btn btn-light" style="margin-top: 30px; margin-left: 300px; font-family: Verdana, Geneva, Tahoma, sans-serif; font-size: medium;"><b>Register/Login</b> </button> </div>
+          </div>
+           <div class="row">
+             <div class="col-sm-12"  style="height: 60px; background-color: rgb(233, 235, 230);">
+              <nav class="navbar navbar-expand-lg navbar-light bg-light"  style="border-radius:8px;border:solid rgb(107, 88, 88); width: 1200px; height: 60px; padding-left: 50px;margin-left: 70px; background-image: linear-gradient(white,rgb(216, 66, 21));">
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#"> <b style="font-family:cursive;">HOME</b><span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#"> <b style="font-family:cursive;">PAGES</b></a>
+                  </li>
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#"> <b style="font-family:cursive;">ABOUT US</b></a>
+                  </li>
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#"> <b style="font-family:cursive;">COURSE</b></a>
+                  </li>
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#"> <b style="font-family:cursive;">CONTACT</b></a>
+                  </li>
+                   </ul>
+                   <img src="{{url('images/cl.jpg')}}" alt="" style="height: 30px; width: 30px; margin-left: 600px;">&nbsp;<h6>6848934235</h6>
+              </div>
+            </nav>
+          </div>
+          
+           </div>
+
+           <div class="row">
+             <div class="col-sm-12" style="height: 500px; background-color: rgb(233, 235, 230);">
+              <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="{{url('images/home1.png')}}" class="d-block w-100" alt="..." style="height: 500px; width: 400px;">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h1 style= "font-family: cursive;background-image: linear-gradient(rgb(158, 136, 136),white); color: black; border-color: darkblue; border-radius: 9px;">All the courses you need</h1>
+                    <p style= "color: black; font-family: cursive; padding-bottom: 300px; font-size: x-large; " ><b>Welcome to Brilliance Academy...</b> </p>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img src="{{url('images/web.jpeg')}}" class="d-block w-100" alt="..."style="height: 500px; width: 300;">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h1 style="font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif; color: blanchedalmond; padding-bottom: 380px;">We Ensure better education for a better world</h1>
+                    
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img src="{{url('images/hom4.jpg')}}" class="d-block w-100" alt="..."style="height: 500px; width: 300px;">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h2 style="font-family: cursive; padding-left: 600PX; padding-bottom: 80PX;">EDUCATION AT IT'S BEST</h2><BR></BR>
+                    
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img src="{{url('images/hom3.png')}}" class="d-block w-100" alt="..."style="height: 500px; width: 300;">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5 style="font-family: cursive; margin-top: 100px;">EXPLORE THE WORLD OF OUR GRADUATES</h5> </BR>
+                    <p style="font-family: cursive;">We develop pioneering leaders & Technologies for the digital age </p>
+                  </div>
+                </div>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div></div>
+           </div>
+         
+      
+
+   </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
